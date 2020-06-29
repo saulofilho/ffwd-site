b4f2207be4214fdc83828b7d3b4c633542fec3dc
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-06-29 16:58:29 -0300

    Update PostSection.js

--- src/components/PostSection.js
+++ src/components/PostSection.js
@@ -11,30 +11,10 @@ const PostSection = ({
       <div className="PostSection">
         <h1>teste 1</h1>
         <div className="post-section-grid">
-          <h1>teste 2</h1>
           {posts.map((post, index) => (
-                    <div className={`post-card`}>
-                    <div className="post-card-text">
-                      <p className="default-text-header">
-                        {post.date}
-                      </p>
                       <p className="default-text-about">
                         {post.PostTitle}
                       </p>
-                    </div>
-                    <div className="post-card-img relative container">
-                      <div 
-                        className="post-card-bg"
-                        style={{
-                          backgroundImage: `url(${post.featuredImage})`
-                        }}
-                        alt={post.PostTitle}
-                      />
-                    </div>
-                    <div className="line container display-none-desk">
-                      <img src={line} alt="line" />
-                    </div>
-                  </div>
           ))}
         </div>
         <div className="anchor-up container">
